c02672e267ac1bb357c6464d0e32a22d03f84920
Tao Wang <moonavw@gmail.com>
2013-02-24 14:38:41 +0800

    move ConvertEx to DynamicQuery proj, since only used by it

--- src/NPatterns.ObjectRelational.DynamicQuery/ConvertEx.cs
+++ src/NPatterns.ObjectRelational.DynamicQuery/ConvertEx.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace NPatterns
+namespace NPatterns.ObjectRelational.DynamicQuery
 {
     /// <summary>
     /// extend the convert with some special convertions
